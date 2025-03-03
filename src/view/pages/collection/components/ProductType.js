886128860e276f30cb3b214a226b11c8f2b8a806
shaileshyadav7039623430@gmail.com <shaileshyadav7039623430@gmail.com>
2025-03-03 18:07:38 +0530

    update contact

--- src/view/pages/collection/components/ProductType.js
+++ src/view/pages/collection/components/ProductType.js
@@ -29,7 +29,7 @@ export const ProductType = () => {
     
     // State to manage form submission status
     const [status, setStatus] = useState('');
-  
+  const [isSubmitted, setIsSubmitted] = useState(false);
     // Handle input changes
     const handleChange = (e) => {
       const { name, value } = e.target;
@@ -43,14 +43,31 @@ export const ProductType = () => {
     const handleSubmit = async (e) => {
       e.preventDefault();
       try {
-        const response = await axios.post(`${process.env.REACT_APP_API_URL}/send-email`, formData);
+        const response = await axios.post(
+          `${process.env.REACT_APP_API_URL}send-email`,
+          {
+            name: formData.name,
+            email: formData.email,
+            message: formData.message,
+            subject: formData.subject,
+          }
+        );
         if (response.status === 200) {
-          setStatus('Message sent successfully!');
+          setStatus("Message sent successfully!");
+          setIsSubmitted(true); // Mark the form as submitted
+  
+          // Reset form data after successful submission
+          setFormData({
+            name: "",
+            email: "",
+            subject: "",
+            message: "",
+          });
         } else {
           setStatus(`Failed to send message. Status code: ${response.status}`);
         }
       } catch (error) {
-        console.error('Error sending message:', error);
+        console.error("Error sending message:", error);
         setStatus(`Failed to send message. Error: ${error.message}`);
       }
     };
@@ -59,56 +76,73 @@ export const ProductType = () => {
         <div className='py-16 lg:px-40 md:px-20 px-5 '>
             <div className='lg:flex justify-center items-center gap-10'>
                <div className='m-auto'> <img className='' src={require("../../../assets/images/4ee8a004-05ab-450e-9ab2-b1e3d9c6f156-0_optimized_100.png")} alt='' data-aos="fade-right" data-aos-duration="2000" data-aos-delay="200" /></div>
-                <div className="lg:w-1/2 md:w-1/2" data-aos="fade-right" data-aos-duration="2000" data-aos-delay="200">
-            <h2 className="tracking-widest lg:text-3xl md:text-3xl text-2xl lg:pt-0 md:pt-0 pt-10">Tell Us What You Need</h2>
-            <form onSubmit={handleSubmit}>
-      <div className="pt-5">
-        <label className="text-666666">Your Name</label>
-        <Input
-          className="mt-1 custinputborder"
-          placeholder="Please Enter First Name"
-          name="name"
-          value={formData.name}
-          onChange={handleChange}
-        />
-      </div>
-      <div className="pt-5">
-        <label className="text-666666">Your Email</label>
-        <Input
-          className="mt-1 custinputborder"
-          placeholder="Please Enter Your Email"
-          name="email"
-          type="email"
-          value={formData.email}
-          onChange={handleChange}
-        />
-      </div>
-      <div className="pt-5">
-        <label className="text-666666">Subject</label>
-        <Input
-          className="mt-1 custinputborder"
-          placeholder="Please Enter Subject"
-          name="subject"
-          value={formData.subject}
-          onChange={handleChange}
-        />
-      </div>
-      <div className="pt-5">
-        <label className="text-666666">Your Message</label>
-        <Textarea
-          className="mt-1 custinputborder"
-          placeholder="Please Enter Your Message"
-          name="message"
-          value={formData.message}
-          onChange={handleChange}
-        />
-      </div>
-      <div className="pt-5">
-        <button type="submit" className="px-6 py-2 rounded-md bg-primary text-white">Send</button>
-      </div>
-    </form>
-            {status && <p className="pt-5 text-green-500">{status}</p>}
-          </div>
+               <div
+                           className="lg:w-1/2 md:w-1/2"
+                           data-aos="fade-right"
+                           data-aos-duration="2000"
+                           data-aos-delay="200"
+                         >
+                           <h2 className="tracking-widest lg:text-3xl md:text-3xl text-2xl">
+                             Tell Us What You Need
+                           </h2>
+                           <form onSubmit={handleSubmit}>
+                             <div className="pt-5">
+                               <label className="text-666666">Your Name</label>
+                               <Input
+                                 className="mt-1 custinputborder"
+                                 placeholder="Please Enter First Name"
+                                 name="name"
+                                 value={formData.name}
+                                 onChange={handleChange}
+                                 disabled={isSubmitted} // Disable input after form submission
+                               />
+                             </div>
+                             <div className="pt-5">
+                               <label className="text-666666">Your Email</label>
+                               <Input
+                                 className="mt-1 custinputborder"
+                                 placeholder="Please Enter Your Email"
+                                 name="email"
+                                 type="email"
+                                 value={formData.email}
+                                 onChange={handleChange}
+                                 disabled={isSubmitted} // Disable input after form submission
+                               />
+                             </div>
+                             <div className="pt-5">
+                               <label className="text-666666">Subject</label>
+                               <Input
+                                 className="mt-1 custinputborder"
+                                 placeholder="Please Enter Subject"
+                                 name="subject"
+                                 value={formData.subject}
+                                 onChange={handleChange}
+                                 disabled={isSubmitted} // Disable input after form submission
+                               />
+                             </div>
+                             <div className="pt-5">
+                               <label className="text-666666">Your Message</label>
+                               <Textarea
+                                 className="mt-1 custinputborder"
+                                 placeholder="Please Enter Your Message"
+                                 name="message"
+                                 value={formData.message}
+                                 onChange={handleChange}
+                                 disabled={isSubmitted} // Disable input after form submission
+                               />
+                             </div>
+                             <div className="pt-5">
+                               <button
+                                 type="submit"
+                                 className="px-6 py-2 rounded-md bg-primary text-white"
+                                 disabled={isSubmitted} // Disable button after form submission
+                               >
+                                 Send
+                               </button>
+                             </div>
+                           </form>
+                           {status && <p className="pt-5 text-green-500">{status}</p>}
+                         </div>
             </div>
 
             <Modal
--- src/view/pages/contact/components/ContactForm.js
+++ src/view/pages/contact/components/ContactForm.js
@@ -1,27 +1,27 @@
-import React, { useState,useRef } from "react";
+import React, { useState } from "react";
 import { Input, Textarea } from "@chakra-ui/react";
 import { Building2, MailIcon, Phone } from "lucide-react";
-// import { Link } from "react-router-dom";
-import axios from "axios"; // If using axios
+import axios from "axios";
 
 export const ContactForm = () => {
   // State to manage form inputs
   const [formData, setFormData] = useState({
-    name: '',
-    email: '',
-    subject: '',
-    message: ''
+    name: "",
+    email: "",
+    subject: "",
+    message: "",
   });
-  
+
   // State to manage form submission status
-  const [status, setStatus] = useState('');
-const ref=useRef();
+  const [status, setStatus] = useState("");
+  const [isSubmitted, setIsSubmitted] = useState(false); // New state for form submission
+
   // Handle input changes
   const handleChange = (e) => {
     const { name, value } = e.target;
     setFormData({
       ...formData,
-      [name]: value
+      [name]: value,
     });
   };
 
@@ -29,14 +29,31 @@ const ref=useRef();
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.post(`${process.env.REACT_APP_API_URL}/send-email`, formData);
+      const response = await axios.post(
+        `${process.env.REACT_APP_API_URL}send-email`,
+        {
+          name: formData.name,
+          email: formData.email,
+          message: formData.message,
+          subject: formData.subject,
+        }
+      );
       if (response.status === 200) {
-        setStatus('Message sent successfully!');
+        setStatus("Message sent successfully!");
+        setIsSubmitted(true); // Mark the form as submitted
+
+        // Reset form data after successful submission
+        setFormData({
+          name: "",
+          email: "",
+          subject: "",
+          message: "",
+        });
       } else {
         setStatus(`Failed to send message. Status code: ${response.status}`);
       }
     } catch (error) {
-      console.error('Error sending message:', error);
+      console.error("Error sending message:", error);
       setStatus(`Failed to send message. Error: ${error.message}`);
     }
   };
@@ -45,80 +62,112 @@ const ref=useRef();
     <div>
       <div className="lg:px-40 md:px-20 px-5 lg:py-20 md:py-10 py-8 container m-auto">
         <div className="lg:flex md:flex gap-10">
-          <div className="lg:w-1/2 md:w-1/2" data-aos="fade-right" data-aos-duration="2000" data-aos-delay="200">
-            <h2 className="tracking-widest lg:text-3xl md:text-3xl text-2xl">Tell Us What You Need</h2>
-            <form ref={ref} action="" onSubmit={handleSubmit}>
-      <div className="pt-5">
-        <label className="text-666666">Your Name</label>
-        <Input
-          className="mt-1 custinputborder"
-          placeholder="Please Enter First Name"
-          name="name"
-          value={formData.name}
-          onChange={handleChange}
-        />
-      </div>
-      <div className="pt-5">
-        <label className="text-666666">Your Email</label>
-        <Input
-          className="mt-1 custinputborder"
-          placeholder="Please Enter Your Email"
-          name="email"
-          type="email"
-          value={formData.email}
-          onChange={handleChange}
-        />
-      </div>
-      <div className="pt-5">
-        <label className="text-666666">Subject</label>
-        <Input
-          className="mt-1 custinputborder"
-          placeholder="Please Enter Subject"
-          name="subject"
-          value={formData.subject}
-          onChange={handleChange}
-        />
-      </div>
-      <div className="pt-5">
-        <label className="text-666666">Your Message</label>
-        <Textarea
-          className="mt-1 custinputborder"
-          placeholder="Please Enter Your Message"
-          name="message"
-          value={formData.message}
-          onChange={handleChange}
-        />
-      </div>
-      <div className="pt-5">
-        <button type="submit" className="px-6 py-2 rounded-md bg-primary text-white">Send</button>
-      </div>
-    </form>
+          <div
+            className="lg:w-1/2 md:w-1/2"
+            data-aos="fade-right"
+            data-aos-duration="2000"
+            data-aos-delay="200"
+          >
+            <h2 className="tracking-widest lg:text-3xl md:text-3xl text-2xl">
+              Tell Us What You Need
+            </h2>
+            <form onSubmit={handleSubmit}>
+              <div className="pt-5">
+                <label className="text-666666">Your Name</label>
+                <Input
+                  className="mt-1 custinputborder"
+                  placeholder="Please Enter First Name"
+                  name="name"
+                  value={formData.name}
+                  onChange={handleChange}
+                  disabled={isSubmitted} // Disable input after form submission
+                />
+              </div>
+              <div className="pt-5">
+                <label className="text-666666">Your Email</label>
+                <Input
+                  className="mt-1 custinputborder"
+                  placeholder="Please Enter Your Email"
+                  name="email"
+                  type="email"
+                  value={formData.email}
+                  onChange={handleChange}
+                  disabled={isSubmitted} // Disable input after form submission
+                />
+              </div>
+              <div className="pt-5">
+                <label className="text-666666">Subject</label>
+                <Input
+                  className="mt-1 custinputborder"
+                  placeholder="Please Enter Subject"
+                  name="subject"
+                  value={formData.subject}
+                  onChange={handleChange}
+                  disabled={isSubmitted} // Disable input after form submission
+                />
+              </div>
+              <div className="pt-5">
+                <label className="text-666666">Your Message</label>
+                <Textarea
+                  className="mt-1 custinputborder"
+                  placeholder="Please Enter Your Message"
+                  name="message"
+                  value={formData.message}
+                  onChange={handleChange}
+                  disabled={isSubmitted} // Disable input after form submission
+                />
+              </div>
+              <div className="pt-5">
+                <button
+                  type="submit"
+                  className="px-6 py-2 rounded-md bg-primary text-white"
+                  disabled={isSubmitted} // Disable button after form submission
+                >
+                  Send
+                </button>
+              </div>
+            </form>
             {status && <p className="pt-5 text-green-500">{status}</p>}
           </div>
-          <div className="lg:w-1/2 md:w-1/2 lg:pt-0 md:pt-0 pt-10" data-aos="fade-left" data-aos-duration="2000" data-aos-delay="200">
+          <div
+            className="lg:w-1/2 md:w-1/2 lg:pt-0 md:pt-0 pt-10"
+            data-aos="fade-left"
+            data-aos-duration="2000"
+            data-aos-delay="200"
+          >
             <div>
-              <h2 className="tracking-widest lg:text-3xl md:text-3xl text-2xl">Contact Us</h2>
+              <h2 className="tracking-widest lg:text-3xl md:text-3xl text-2xl">
+                Contact Us
+              </h2>
             </div>
             <div className="pt-5">
               <div className="flex items-center gap-2">
                 <Building2 className="lg:w-5 md:w-0 w-5 lg:h-5 md:h-0 h-5" />
                 <p className="lg:text-xl md:text-xl text-lg">Address</p>
               </div>
-              <p className="pt-2 text-666666">Gemasia B.V.B.A Hoveniersstraat 53,<br/> Office 314 P.Box 49 2018  <br/>Antwerp, Belgium</p>
+              <p className="pt-2 text-666666">
+                Gemasia B.V.B.A Hoveniersstraat 53,<br />
+                Office 314 P.Box 49 2018 <br />
+                Antwerp, Belgium
+              </p>
             </div>
             <div className="flex items-center gap-2 pt-5">
               <Phone className="lg:w-5 md:w-0 w-5 lg:h-5 md:h-0 h-5" />
               <p className="lg:text-xl md:text-xl text-lg">Phone</p>
             </div>
             <div className="pt-2">
-              <p className="text-666666 text-hover"><a href="tel:+32 32327580">Tel:+32 32327580</a></p>
+              <p className="text-666666 text-hover">
+                <a href="tel:+32 32327580">Tel:+32 32327580</a>
+              </p>
             </div>
             <div className="flex items-center gap-2 pt-5">
               <MailIcon className="lg:w-5 md:w-0 w-5 lg:h-5 md:h-0 h-5" />
               <p className="lg:text-xl md:text-xl text-lg">Email</p>
             </div>
             <div className="pt-2">
-              <p className="text-666666 text-hover"><a href="mailto:avnit@gemasiagroup.com">avnit@gemasiagroup.com</a></p>
+              <p className="text-666666 text-hover">
+                <a href="mailto:avnit@gemasiagroup.com">avnit@gemasiagroup.com</a>
+              </p>
             </div>
           </div>
         </div>
